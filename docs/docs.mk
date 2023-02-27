1f7f5daa3cb59fb4f4d3835e587caf6726bd9a50
Robert Jenssen <DesignOfIIRFilters@gmail.com>
2023-02-27 13:38:34 +1100

    Add files to .gitignore

--- docs/docs.mk
+++ docs/docs.mk
@@ -6,13 +6,6 @@ DOCUMENTATION += librj
 cleandocs:
     
 
-.PHONY: gitignore
-gitignore:
-	-rm -f .gitignore
-	echo $(CLEAN_SUFFIXES:%="*"%) > .gitignore
-	echo $(CLEAN_FILES) >> .gitignore
-	sed -i -e "s/\ /\n/g" .gitignore
-
 #
 # For github and jekyll
 #
@@ -27,6 +20,6 @@ cleanjekyll:
 jekyll: clean cleanjekyll
 	jekyll serve $(JEKYLL_OPTS)
 
-CLEAN_DIRS += $(addprefix $(DOCS), \
+CLEAN_DIRS += $(addprefix $(DOCS)/, \
 _site .sass-cache .jekyll-cache .jekyll-metadata)
 
