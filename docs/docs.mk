1a7480e2f68abd761c2750b81cb67f57d47f2b50
Robert Jenssen <DesignOfIIRFilters@gmail.com>
2023-03-05 15:18:31 +1100

    Add docs/docs.rules

--- docs/docs.mk
+++ docs/docs.mk
@@ -1,25 +1,7 @@
+include docs/docs.rules
+include docs/jekyll.rules
 include docs/doxygen/doxygen.mk
 
+DOCUMENTATION_TYPES += doxygen
 DOCUMENTATION += librj
-
-.PHONY: cleandocs
-cleandocs:
-    
-
-#
-# For github and jekyll
-#
-JEKYLL_OPTS=--config $(DOCS)/_config.yml --source $(DOCS) \
---destination $(DOCS)/_site
-
-.PHONY: cleanjekyll
-cleanjekyll:    
-	jekyll clean $(JEKYLL_OPTS)
-
-.PHONY: jekyll
-jekyll: clean cleanjekyll
-	jekyll serve $(JEKYLL_OPTS)
-
-CLEAN_DIRS += $(addprefix $(DOCS)/, \
-_site .sass-cache .jekyll-cache .jekyll-metadata)
 
